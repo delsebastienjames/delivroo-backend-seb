042af5491fb58a1cddddce3c25fe47a3e7c32639
Sebastien <delsebastienjames@gmail.com>
2021-05-04 20:27:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/", (req, res) => {
             id: "1519055545-88",
             title: "Brunch authentique 1 personne",
             description:
-              "Assiette de jambon cuit, jambon fumeì, terrine, comté bio & camembert bio, salade jeunes pousses, oeuf poché bio, pain bio & confiture, 1 viennoiserie bio au choix, granola parfait bio, jus de fruits 33cl au choix",
+              "Assiette de jambon cuit, jambon fumé, terrine, comté bio & camembert bio, salade jeunes pousses, oeuf poché bio, pain bio & confiture, 1 viennoiserie bio au choix, granola parfait bio, jus de fruits 33cl au choix",
             price: "25.00",
             picture:
               "https://f.roocdn.com/images/menu_items/1583350/item-image.jpg",
